7fb4c79fab3cef76925c4d6867437057b40eddc7
Plant Genius <retnoadisaputra57@gmail.com>
2023-05-20 12:08:00 +0700

    deases

---
+++ app/src/main/java/com/dicoding/plantgenius/main/Deases.kt
@@ -0,0 +1,13 @@
+package com.dicoding.plantgenius.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.plantgenius.R
+
+class Deases : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deases)
+    }
+}
